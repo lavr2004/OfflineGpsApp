b2dcf6f249793be16e7f23f7e8bf7305c3e9a199
Andrei Ihnatovicz <lavr2004@gmail.com>
2025-04-30 07:04:19 +0200

    20250430 - UPDATE: reading coordinates from GPX file and calculation MAX bounds used later for map tiles downloading

---
+++ OfflineGpsApp/CodeBase/OpenStreetMapLocalTileSource.cs
@@ -0,0 +1,26 @@
+﻿using BruTile.Predefined;
+using BruTile;
+
+namespace OfflineGpsApp.CodeBase
+{
+    public class OpenStreetMapLocalTileSource : ITileSource//BruTile
+    {
+        //BruTile.ITileSource interface requirements that TileLayer needs to implement
+
+        public ITileSchema Schema { get; } = new GlobalSphericalMercator();
+        public string Name { get; } = "Local OSM Tiles";
+        public Attribution Attribution { get; } = new Attribution("© OpenStreetMap");
+
+        public async Task<byte[]?> GetTileAsync(TileInfo oTileInfo)
+        {
+            //path: tiles/0/0/0.png
+            var tilePath = Path.Combine(FileSystem.AppDataDirectory, "tiles", oTileInfo.Index.Level.ToString(), oTileInfo.Index.Col.ToString(), $"{oTileInfo.Index.Row}.png");
+
+            if (File.Exists(tilePath))
+            {
+                return await File.ReadAllBytesAsync(tilePath);
+            }
+            return null;
+        }
+    }
+}
